cee90106e11da654b701cb2272c0d7054df75211
Xamarr <panthelondan@gmail.com>
2022-09-08 15:57:47 -0700

    0-4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-gets_to_print
+ *
+ * Return: should be 0 (means its sucessful)
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
