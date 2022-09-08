144420f8e57ec60b906107e20532186b170bf0f8
Xamarr <panthelondan@gmail.com>
2022-09-08 16:32:04 -0700

    5-6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - should print a text
+ *
+ * Return: should be 0 (means its sucessful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return(0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ *
+ * main - prints the diff var sizes
+ *
+ * Return: should be 0 (success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+
+	printf("Size of char: %d byte(s)\n",sizeof(a));
+	printf("Size of int: %d byte(s)\n",sizeof(b));
+	printf("Size of long int: %d byte((s)\n",sizeof(c));
+	printf("Size of long long int: %d byte(s)\n",sizeof(d));
+	printf("Size of float: %d byte(s)\n",sizeof(e));
+	return (0);
+}
