a750e6c39e8e40420023458719d51b6e6c5e68cd
Xamarr <panthelondan@gmail.com>
2022-09-11 03:31:45 -0700

    4-5

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -18,6 +18,6 @@ int main(void)
 			putchar(noQE);
 		}
 	}
-	putchar(\n);
+	putchar('\n');
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ * main - prints a number of integers
+ *
+ * Description: the program prints numbers
+ * with a new line after each
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+		printf("%d\n", n);
+
+	return (0);
+}
