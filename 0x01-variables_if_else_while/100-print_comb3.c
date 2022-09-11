3931f7f6c1a69d884db86e137939915bb547eecf
Xamarr <panthelondan@gmail.com>
2022-09-11 07:15:31 -0700

    9-10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,32 @@
 #include <stdio.h>
+/**
+ * main- print a combo of digits
+ *
+ * Result: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = o ; i <= 9 ; i++)
+	{
+		for (j = 0 ; i <= 9 ; j++)
+		{
+			if (j > i)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+
+				if (i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
