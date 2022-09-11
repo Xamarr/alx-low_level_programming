1b3c31861b65f6b3721e676b5d7738608eec0cbe
Xamarr <panthelondan@gmail.com>
2022-09-11 08:13:19 -0700

    10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,26 +7,42 @@
 
 int main(void)
 {
-	int i,j;
+	int i, j;
 
 
 	for (i = 0; i <= 9; i++)
 	{
+
 		for (j = 1 ; i <= 9 ; j++)
+
 		{
+
 			if (j > i)
+
 			{
+
 				putchar(i + '0');
+
 				putchar(j + '0');
 
 				if (i != 8)
+
 				{
+
 					putchar(',');
+
+
 					putchar(' ');
+
 				}
+
 			}
+
 		}
+	
 	}
+
 	putchar('\n');
+
 	return (0);
 }
