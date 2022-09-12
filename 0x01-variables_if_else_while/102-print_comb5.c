336d570fbecf42e730dd44e87c5b5dacc1285296
Xamarr <panthelondan@gmail.com>
2022-09-11 20:04:03 -0700

    11-12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,2 +1,31 @@
 #include <stdio.h>
+/**
+ * main - print all combo of 2 digits
+ *
+ * Return: 0 (sucessful)
+ */
+int main(void)
+{
+	int i, p;
 
+	for (i = 0; i < 100; i++)
+	{
+		if (p > i)
+		{
+			putchar((i / 10) + '0');
+
+			putchar((i % 10) + '0');
+
+			putchar(' ');
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+			if (i != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
